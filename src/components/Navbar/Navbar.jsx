a8717b79986c28ce26ad9bd93e7a3c381ed63ed5
Thobbie Egunjobi <egunjobithobbie4@gmail.com>
2025-12-23 19:07:25 +0100

    Updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import './Navbar.css'
 import logo from '../../assets/spent difital labs BLACK 1.png'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav>
         <img src={logo} className='logo' alt=""  />
         <ul>
-            <li>About Us</li>
-            <li>Research</li>
-            <li>Innovations</li>
-            <li>Education</li>
-            <li>Pulications</li>
-            <li>Contact</li>
+            <li><Link>About Us</Link></li>
+            <li><Link>Research</Link></li>
+            <li><Link>Innovations</Link></li>
+            <li><Link>Education</Link></li>
+            <li><Link>Publications</Link></li>
+            <li><Link>Contact</Link></li>
             <li><button className='btn'>Join our Research Community</button></li>
         </ul>
     </nav>
